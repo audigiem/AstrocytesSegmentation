[NEW][FIX] fix amplitude image and features computation, pipeline performs as the Java one, small differencies due to closing morphollogy and median filter

astroca/croppingBoundaries/computeBoundaries.py
@@ -11,8 +11,10 @@ import numpy as np
 from astroca.tools.exportData import export_data, save_numpy_tab 
 import os
 from tqdm import tqdm
+from typing import List, Dict, Tuple, Any
 
-def compute_boundaries(data: np.ndarray, params: dict) -> tuple:
+
+def compute_boundaries(data: np.ndarray, params: dict) -> Tuple[np.ndarray, np.ndarray, float, np.ndarray]:
     """
     Compute cropping boundaries in X for each Z slice based on background values.
     Sets boundary pixels to default_value and saves results optionally.

astroca/dynamicImage/dynamicImage.py
@@ -82,11 +82,12 @@ def compute_dynamic_image(data: np.ndarray,
     return dF, mean_noise
 
 
-def compute_image_amplitude(data_cropped, index_xmin: np.ndarray, index_xmax: np.ndarray, param_values: dict) -> np.ndarray:
+def compute_image_amplitude(data_cropped: np.ndarray, F0: np.ndarray, index_xmin: np.ndarray, index_xmax: np.ndarray, param_values: dict) -> np.ndarray:
     """
     @brief Compute the amplitude of the image using the Anscombe inverse transform.
     result -> (data_cropped - f0_inv)/f0_inv
     @param data_cropped: 4D numpy array of shape (T, Z, Y, X) representing the cropped raw data.
+    @param F0: 4D numpy array of shape (1, Z, Y, X) representing the background estimation to be inversed.
     @param index_xmin: 1D array of shape (depth,) with left cropping bounds per z
     @param index_xmax: 1D array of shape (depth,) with right cropping bounds per z
     @param param_values: Dictionary containing the parameters:
@@ -101,17 +102,32 @@ def compute_image_amplitude(data_cropped, index_xmin: np.ndarray, index_xmax: np
     save_results = int(param_values['files']['save_results']) == 1
     output_directory = param_values['paths']['output_dir']
 
-    f0_inv = anscombe_inverse(data_cropped.copy(), index_xmin, index_xmax, param_values=param_values)
+    f0_inv = anscombe_inverse(F0, index_xmin, index_xmax, param_values=param_values)
 
-    # prevent division by zero
-    safe_f0_inv = np.where(f0_inv == 0, 1e-10, f0_inv)
-    image_amplitude = (data_cropped - f0_inv) / safe_f0_inv
+    T, Z, Y, X = data_cropped.shape
+    image_amplitude = np.zeros_like(data_cropped, dtype=np.float32)
+
+    for z in tqdm(range(Z), desc="Computing amplitude per Z-slice", unit="slice"):
+        x_min = index_xmin[z]
+        x_max = index_xmax[z] + 1
+
+        if x_min >= x_max:
+            continue
+
+        f0_slice = f0_inv[0, z, :, x_min:x_max]
+        f0_safe = np.where(f0_slice == 0, 1e-10, f0_slice)
+
+        for t in range(T):
+            data_slice = data_cropped[t, z, :, x_min:x_max]
+            result_slice = (data_slice - f0_slice) / f0_safe
+            image_amplitude[t, z, :, x_min:x_max] = result_slice
 
     if save_results:
         if output_directory is None:
             raise ValueError("Output directory must be specified when save_results is True.")
         os.makedirs(output_directory, exist_ok=True)
         export_data(image_amplitude, output_directory, export_as_single_tif=True, file_name="image_amplitude")
-    print(60*"=")
+
+    print(60 * "=")
     print()
     return image_amplitude

astroca/events/eventDetectorCorrected.py
@@ -338,11 +338,15 @@ class EventDetectorOptimized:
             self._process_small_groups(small_AV_groups, id_small_AV_groups)
 
         print(f"\nTotal events found: {len(self.final_id_events_)}")
-        print(f"Size of each final event:")
-        for event_id in self.final_id_events_:
-            size = np.sum(self.id_connected_voxel_ == event_id)
-            print(f"    Event ID={event_id}: {size} voxels")
+        # print(f"Size of each final event:")
+        # for event_id in self.final_id_events_:
+        #     size = np.sum(self.id_connected_voxel_ == event_id)
+        #     print(f"    Event ID={event_id}: {size} voxels")
         self._compute_final_id_events()
+        # # test pour voir si le renommage fonctionne
+        # for event in range(1,len(self.final_id_events_)+1):
+        #     size = np.sum(self.id_connected_voxel_ == event)
+        #     print(f"    Final Event ID={event}: {size} voxels")
 
         print(f"Total time taken: {time.time() - start_time:.2f} seconds")
 
@@ -673,7 +677,7 @@ def detect_calcium_events_opti(av_data: np.ndarray, params_values: dict = None,
 
     print(60 * "=")
     print()
-    return id_connections, id_events
+    return id_connections, len(id_events)
 
 
 def test_with_synthetic_data():

astroca/features/featuresComputation.py
@@ -9,7 +9,7 @@ import os
 from tqdm import tqdm
 
 
-def save_features_from_events(calcium_events: np.ndarray, events_ids: list, image_amplitude: np.ndarray, params_values: dict=None) -> None:
+def save_features_from_events(calcium_events: np.ndarray, events_ids: int, image_amplitude: np.ndarray, params_values: dict=None) -> None:
     """
     @brief Compute features from calcium events in a 3D image sequence with time dimension and save them to an Excel file.
         - duration: nb of frames of the event
@@ -19,7 +19,7 @@ def save_features_from_events(calcium_events: np.ndarray, events_ids: list, imag
         - centroïd: coordinates of the center of mass of the event
         - classification: classification of the event (wave, local, etc.)
     @param calcium_events: 4D numpy array of shape (T, Z, Y, X) representing the calcium events.
-    @param events_ids: List of unique event IDs from the calcium events data.
+    @param events_ids: Number of unique event IDs from the calcium events data.
     @param image_amplitude: 4D numpy array of shape (T, Z, Y, X) representing the amplitude of the image.
     @param params_values: Dictionary containing parameters for feature computation:
         - voxel_size_x, voxel_size_y, voxel_size_z: size of a voxel in micrometers.
@@ -49,7 +49,7 @@ def save_features_from_events(calcium_events: np.ndarray, events_ids: list, imag
     print(60*"=")
 
 
-def compute_features(calcium_events: np.ndarray, events_ids: list, image_amplitude: np.ndarray, params_values: dict=None) -> dict:
+def compute_features(calcium_events: np.ndarray, events_ids: int, image_amplitude: np.ndarray, params_values: dict=None) -> dict:
     """
     @brief Compute features from calcium events in a 3D image sequence with time dimension.
         - duration: nb of frames of the event
@@ -57,10 +57,9 @@ def compute_features(calcium_events: np.ndarray, events_ids: list, image_amplitu
         - amplitude: amplitude if the intensity of the event
         - volume: volume of the event in micrometers^3
         - centroïd: coordinates of the center of mass of the event
-        -classification: classification of the event (wave, local, etc.)
-
+        - classification: classification of the event (wave, local, etc.)
     @param calcium_events: 4D numpy array of shape (T, Z, Y, X) representing the calcium events.
-    @param events_ids: List of unique event IDs from the calcium events data.
+    @param events_ids: Number of unique event IDs from the calcium events data.
     @param image_amplitude: 4D numpy array of shape (T, Z, Y, X) representing the amplitude of the image.
     @param params_values: Dictionary containing parameters for feature computation:
         - voxel_size_x, voxel_size_y, voxel_size_z: size of a voxel in micrometers.
@@ -83,7 +82,7 @@ def compute_features(calcium_events: np.ndarray, events_ids: list, image_amplitu
 
     features = {}
 
-    for event_id in tqdm(events_ids, desc="Computing features per event"):
+    for event_id in tqdm(range(1, events_ids + 1), desc="Computing features per event"):
         event_mask = (calcium_events == event_id)
         if not np.any(event_mask):
             continue
@@ -93,7 +92,10 @@ def compute_features(calcium_events: np.ndarray, events_ids: list, image_amplitu
         t0 = coords[:, 0].min()
         t1 = coords[:, 0].max()
         duration = t1 - t0 + 1
-        centroid = np.mean(coords, axis=0)
+        centroid_x = coords[:, 3].mean().astype(int)
+        centroid_y = coords[:, 2].mean().astype(int)
+        centroid_z = coords[:, 1].mean().astype(int)
+        centroid_t = coords[:, 0].mean().astype(int)
         volume = nb_voxels * np.prod(voxel_size)
         amplitude = np.max(image_amplitude[event_mask])
 
@@ -103,17 +105,20 @@ def compute_features(calcium_events: np.ndarray, events_ids: list, image_amplitu
         else:
             if volume <= volume_localized:
                 class_label = "localized"
-                confidence = 1.0
+                confidence = 100
             else:
                 class_label = "localized but not microdomain"
-                confidence = 1.0
+                confidence = 100
 
         features[event_id] = {
             'duration': duration,
             't0': t0,
             'amplitude': amplitude,
             'volume': volume,
-            'centroid': centroid,
+            'centroid_x': centroid_x,
+            'centroid_y': centroid_y,
+            'centroid_z': centroid_z,
+            'centroid_t': centroid_t,
             'classification': class_label,
             'confidence': confidence
         }
@@ -157,7 +162,7 @@ def write_excel_features(features: dict, output_directory: str) -> None:
     @param output_directory: Directory to save the Excel file.
     """
     df = pd.DataFrame.from_dict(features, orient='index')
-    output_file = f"{output_directory}/calcium_events_features.xlsx"
+    output_file = f"{output_directory}calcium_events_features.xlsx"
     df.to_excel(output_file, index_label='Event ID')
     print(f"Features saved to {output_file}")
 
@@ -170,6 +175,6 @@ def write_csv_features(features: dict, output_directory: str) -> None:
     @param output_directory: Directory to save the CSV file.
     """
     df = pd.DataFrame.from_dict(features, orient='index')
-    output_file = f"{output_directory}/calcium_events_features.csv"
+    output_file = f"{output_directory}calcium_events_features.csv"
     df.to_csv(output_file, index_label='Event ID')
     print(f"Features saved to {output_file}")

astroca/varianceStabilization/varianceStabilization.py
@@ -40,6 +40,7 @@ def compute_variance_stabilization(data: np.ndarray,
     output_directory = params['paths']['output_dir']
     
     T, Z, Y, X = data.shape
+    data = data.astype(np.float32)
 
     for z in tqdm(range(Z), desc="Variance stabilization per Z-slice", unit="slice"):
         x_min, x_max = index_xmin[z], index_xmax[z] + 1
@@ -92,21 +93,26 @@ def anscombe_inverse(data: np.ndarray, index_xmin: np.ndarray, index_xmax: np.nd
     """
     Compute inverse of Anscombe transform to compute the amplitude of the image
         A⁻¹(x) = (x / 2)^2 - 3/8
+
     @param data: 4D numpy array of shape (T, Z, Y, X)
-    @param index_xmin: 1D array of shape (depth,) with left cropping bounds per z
-    @param index_xmax:1D array of shape (depth,) with right cropping bounds per z
+    @param index_xmin: 1D array of shape (Z,) with left cropping bounds per z
+    @param index_xmax: 1D array of shape (Z,) with right cropping bounds per z
     @param param_values: Dictionary containing the parameters:
         - save_results: If True, saves the result to output_directory
         - output_directory: Directory to save the result if save_results is True
-    @return:
+    @return: Transformed image of same shape as input
     """
-    print(" - Applying inverse Anscombe transform...")
+    print(" - Applying inverse Anscombe transform on 3D volume...")
     required_keys = {'files', 'paths'}
     if not required_keys.issubset(param_values.keys()):
         raise ValueError(f"Missing required parameters: {required_keys - param_values.keys()}")
+
     save_results = int(param_values['files']['save_results']) == 1
     output_directory = param_values['paths']['output_dir']
-    T, Z, Y, X = data.shape
+    _, Z, Y, X = data.shape
+    data = data.astype(np.float32)
+
+    data_out = np.zeros_like(data, dtype=np.float32)
 
     for z in tqdm(range(Z), desc="Inverse Anscombe per Z-slice", unit="slice"):
         x_min = index_xmin[z]
@@ -115,16 +121,19 @@ def anscombe_inverse(data: np.ndarray, index_xmin: np.ndarray, index_xmax: np.nd
         if x_min >= x_max:
             continue
 
-        sub_volume = data[:, z, :, x_min:x_max]
-        sub_volume /= 2.0
-        np.square(sub_volume, out=sub_volume)
-        sub_volume -= 3.0 / 8.0
+        slice_section = data[0, z, :, x_min:x_max]
+        slice_out = data_out[0, z, :, x_min:x_max]
+
+        # Apply A⁻¹(x) = (x/2)^2 - 3/8
+        np.divide(slice_section, 2.0, out=slice_out)
+        np.square(slice_out, out=slice_out)
+        slice_out -= 3.0 / 8.0
 
     if save_results:
         if output_directory is None:
             raise ValueError("Output directory must be specified when save_results is True.")
         os.makedirs(output_directory, exist_ok=True)
-        export_data(data, output_directory, export_as_single_tif=True,
-                    file_name="inverse_anscombe_transformed_sequence")
+        export_data(data_out, output_directory, export_as_single_tif=True,
+                    file_name="inverse_anscombe_transformed_volume")
 
-    return data
+    return data_out